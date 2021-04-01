e93b9f26cfc61d8c0850ec862841958f2c7c5c4c
akinwale <4412196+dewhallez@users.noreply.github.com>
2021-04-01 01:00:06 -0400

    add login_required to views.py

--- web_blogs/models.py
+++ web_blogs/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Topic(models.Model):
     """A topic the user is learning about"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
-
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
         """Return  a string representation of the model."""
@@ -23,6 +23,6 @@ class Entry(models.Model):
 
     def __str__(self):
         """Return a string representation of the model."""
-        return f"{self.text[:50]}....."
+        return f"{self.text[:40]}....."
 
 
--- web_blogs/views.py
+++ web_blogs/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+
+
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -8,12 +12,14 @@ def index(request):
     """The home page for Learning Log."""
     return render(request, 'web_blogs/index.html')
 
+@login_required
 def topics(request):
     """Show all topics."""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'web_blogs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
@@ -21,7 +27,7 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'web_blogs/topic.html', context)
 
-
+@login_required
 def new_topic(request):
     """Add a new topic."""
     if request.method != 'POST':
@@ -31,14 +37,16 @@ def new_topic(request):
         # Post data submitted; process data
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('web_blogs:topics')
 
     # Display invalid form or blank form
     context = {'form': form}
     return render(request, 'web_blogs/new_topic.html', context)
 
-
+@login_required
 def new_entry(request, topic_id):
     """Add new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
@@ -59,11 +67,13 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'web_blogs/new_entry.html', context)
 
-
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry
